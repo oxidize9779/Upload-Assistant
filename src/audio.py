eacce69cc146943d9736ca75610aedfbfd44cadd
oxidize9779 <230537028+oxidize9779@users.noreply.github.com>
2025-09-06 19:05:34 +1000

    Split out channel and audio detection

--- src/audio.py
+++ src/audio.py
@@ -3,6 +3,122 @@ import traceback
 from src.console import console
 
 
+def get_channels(track):
+    channels = track.get('Channels_Original', track.get('Channels'))
+    additional_features = track.get('Format_AdditionalFeatures', '')
+
+    if not str(channels).isnumeric():
+        channels = track.get('Channels')
+    try:
+        channel_layout = track.get('ChannelLayout', '') or track.get('ChannelLayout_Original', '') or track.get('ChannelPositions', '')
+    except Exception:
+        channel_layout = ''
+
+    if (channel_layout and "LFE" in channel_layout) or additional_features and additional_features.endswith("X"):
+        chan = f"{int(channels) - 1}.1"
+    elif channel_layout == "":
+        if int(channels) <= 2:
+            chan = f"{int(channels)}.0"
+        else:
+            chan = f"{int(channels) - 1}.1"
+    else:
+        chan = f"{channels}.0"
+
+    return chan
+
+
+def get_codec(commercial_name, additional_features, format, format_settings, format_profile, channels):
+    # Convert commercial name to naming conventions
+    audio = {
+        "DTS": "DTS",
+        "AAC": "AAC",
+        "AAC LC": "AAC",
+        "AC-3": "DD",
+        "E-AC-3": "DD+",
+        "A_EAC3": "DD+",
+        "Enhanced AC-3": "DD+",
+        "MLP FBA": "TrueHD",
+        "FLAC": "FLAC",
+        "Opus": "Opus",
+        "Vorbis": "VORBIS",
+        "PCM": "LPCM",
+        "LPCM Audio": "LPCM",
+        "Dolby Digital Audio": "DD",
+        "Dolby Digital Plus Audio": "DD+",
+        "Dolby Digital Plus": "DD+",
+        "Dolby TrueHD Audio": "TrueHD",
+        "DTS Audio": "DTS",
+        "DTS-HD Master Audio": "DTS-HD MA",
+        "DTS-HD High-Res Audio": "DTS-HD HRA",
+        "DTS:X Master Audio": "DTS:X"
+    }
+    audio_extra = {
+        "XLL": "-HD MA",
+        "XLL X": ":X",
+        "ES": "-ES",
+    }
+    format_extra = {
+        "JOC": " Atmos",
+        "16-ch": " Atmos",
+        "Atmos Audio": " Atmos",
+    }
+    format_settings_extra = {
+        "Dolby Surround EX": "EX"
+    }
+    commercial_names = {
+        "Dolby Digital": "DD",
+        "Dolby Digital Plus": "DD+",
+        "Dolby TrueHD": "TrueHD",
+        "DTS-ES": "DTS-ES",
+        "DTS-HD High": "DTS-HD HRA",
+        "Free Lossless Audio Codec": "FLAC",
+        "DTS-HD Master Audio": "DTS-HD MA"
+    }
+
+    extra = ""
+    search_format = True
+
+    if isinstance(additional_features, dict):
+        additional_features = ""  # Set empty string if additional_features is a dictionary
+
+    if commercial_name:
+        for key, value in commercial_names.items():
+            if key in commercial_name:
+                codec = value
+                search_format = False
+            if "Atmos" in commercial_name or format_extra.get(additional_features, "") == " Atmos":
+                extra = " Atmos"
+
+    if search_format:
+        codec = audio.get(format, "") + audio_extra.get(additional_features, "")
+        extra = format_extra.get(additional_features, "")
+
+    format_settings = format_settings_extra.get(format_settings, "")
+    if format_settings == "EX" and channels == "5.1":
+        format_settings = "EX"
+    else:
+        format_settings = ""
+
+    if codec == "":
+        codec = format
+
+    if format.startswith("DTS"):
+        if additional_features and additional_features.endswith("X"):
+            codec = "DTS:X"
+
+    if format == "MPEG Audio":
+        if format_profile == "Layer 2":
+            codec = "MP2"
+        elif format_profile == "Layer 3":
+            codec = "MP3"
+
+    if codec == "DD" and channels == "7.1":
+        console.print("[warning] Detected codec is DD but channel count is 7.1, correcting to DD+")
+        codec = "DD+"
+
+    return codec, extra
+
+
 async def get_audio_v2(mi, meta, bdinfo):
     extra = dual = ""
     has_commentary = False
@@ -47,25 +163,10 @@ async def get_audio_v2(mi, meta, bdinfo):
         if format_settings in ['Explicit']:
             format_settings = ""
         format_profile = track.get('Format_Profile', '')
-        # Channels
-        channels = track.get('Channels_Original', track.get('Channels'))
-        if not str(channels).isnumeric():
-            channels = track.get('Channels')
-        try:
-            channel_layout = track.get('ChannelLayout', '') or track.get('ChannelLayout_Original', '') or track.get('ChannelPositions', '')
-        except Exception:
-            channel_layout = ''
 
-        if channel_layout and "LFE" in channel_layout:
-            chan = f"{int(channels) - 1}.1"
-        elif channel_layout == "":
-            if int(channels) <= 2:
-                chan = f"{int(channels)}.0"
-            else:
-                chan = f"{int(channels) - 1}.1"
-        else:
-            chan = f"{channels}.0"
+        chan = get_channels(track)
 
+    if format.startswith("DTS"):
         if meta.get('dual_audio', False):
             dual = "Dual-Audio"
         else:
@@ -141,94 +242,7 @@ async def get_audio_v2(mi, meta, bdinfo):
                 console.print(traceback.format_exc())
                 pass
 
-    # Convert commercial name to naming conventions
-    audio = {
-        "DTS": "DTS",
-        "AAC": "AAC",
-        "AAC LC": "AAC",
-        "AC-3": "DD",
-        "E-AC-3": "DD+",
-        "A_EAC3": "DD+",
-        "Enhanced AC-3": "DD+",
-        "MLP FBA": "TrueHD",
-        "FLAC": "FLAC",
-        "Opus": "Opus",
-        "Vorbis": "VORBIS",
-        "PCM": "LPCM",
-        "LPCM Audio": "LPCM",
-        "Dolby Digital Audio": "DD",
-        "Dolby Digital Plus Audio": "DD+",
-        "Dolby Digital Plus": "DD+",
-        "Dolby TrueHD Audio": "TrueHD",
-        "DTS Audio": "DTS",
-        "DTS-HD Master Audio": "DTS-HD MA",
-        "DTS-HD High-Res Audio": "DTS-HD HRA",
-        "DTS:X Master Audio": "DTS:X"
-    }
-    audio_extra = {
-        "XLL": "-HD MA",
-        "XLL X": ":X",
-        "ES": "-ES",
-    }
-    format_extra = {
-        "JOC": " Atmos",
-        "16-ch": " Atmos",
-        "Atmos Audio": " Atmos",
-    }
-    format_settings_extra = {
-        "Dolby Surround EX": "EX"
-    }
-
-    commercial_names = {
-        "Dolby Digital": "DD",
-        "Dolby Digital Plus": "DD+",
-        "Dolby TrueHD": "TrueHD",
-        "DTS-ES": "DTS-ES",
-        "DTS-HD High": "DTS-HD HRA",
-        "Free Lossless Audio Codec": "FLAC",
-        "DTS-HD Master Audio": "DTS-HD MA"
-    }
-
-    search_format = True
-
-    if isinstance(additional, dict):
-        additional = ""  # Set empty string if additional is a dictionary
-
-    if commercial:
-        for key, value in commercial_names.items():
-            if key in commercial:
-                codec = value
-                search_format = False
-            if "Atmos" in commercial or format_extra.get(additional, "") == " Atmos":
-                extra = " Atmos"
-
-    if search_format:
-        codec = audio.get(format, "") + audio_extra.get(additional, "")
-        extra = format_extra.get(additional, "")
-
-    format_settings = format_settings_extra.get(format_settings, "")
-    if format_settings == "EX" and chan == "5.1":
-        format_settings = "EX"
-    else:
-        format_settings = ""
-
-    if codec == "":
-        codec = format
-
-    if format.startswith("DTS"):
-        if additional and additional.endswith("X"):
-            codec = "DTS:X"
-            chan = f"{int(channels) - 1}.1"
-
-    if format == "MPEG Audio":
-        if format_profile == "Layer 2":
-            codec = "MP2"
-        elif format_profile == "Layer 3":
-            codec = "MP3"
-
-    if codec == "DD" and chan == "7.1":
-        console.print("[warning] Detected codec is DD but channel count is 7.1, correcting to DD+")
-        codec = "DD+"
+    codec, extra = get_codec(commercial, additional, format, format_settings, format_profile, chan)
 
     audio = f"{dual} {codec or ''} {format_settings or ''} {chan or ''}{extra or ''}"
     audio = ' '.join(audio.split())
